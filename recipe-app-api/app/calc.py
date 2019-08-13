654b499c6fb8ffd55e1d43c54441392d7c02d146
Tushar Kaushik <tusharkaushik036@gmail.com>
2019-08-13 12:04:29 +0530

    delete unrequired files

--- recipe-app-api/app/calc.py
+++ recipe-app-api/app/calc.py
@@ -1,3 +1,6 @@
 def add(x,y):
     """add two numbers together"""
     return x+y
+
+def subtract(x,y):
+    return x-y
--- recipe-app-api/app/tests.py
+++ recipe-app-api/app/tests.py
@@ -1,9 +1,15 @@
 from django.test import TestCase
 
 from app.calc import add
+from app.calc import subtract
 
 class CalcTests(TestCase):
 
     def test_add_numbers(self):
         """Test that 2 numbers are added together"""
         self.assertEqual(add(3,8), 11)
+
+
+    def test_subtract_numbers(self):
+        """test that values are substracted and return"""
+        self.assertEqual(subtract(11,5), 6)
